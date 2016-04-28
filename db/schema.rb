21ad7be26eb94dd893939d841373061fcb32d211
Kim Foale <kim@alliscalm.net>
2016-04-28 12:59:16 +0100

    Reset css to defaults

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428112651) do
+ActiveRecord::Schema.define(version: 20160428114635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "type"
+    t.datetime "date"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "members", force: :cascade do |t|
     t.string   "name"
