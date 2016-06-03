56acbd72a9c61f4e5b5775ab2df788d5dec91be3
Kim Foale <kim@alliscalm.net>
2016-06-03 16:34:34 +0100

    Create geolocation

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,17 +3,21 @@ class Event < ActiveRecord::Base
   validates_uniqueness_of :google_id, :allow_blank => true, :allow_nil => true
   enum category: [ :research, :project, :partnership ]
 
+  # after_save :geolocate
+
   POSTCODE_REGEX = "(GIR 0AA)|((([A-Z-[QVX]][0-9][0-9]?)|(([A-Z-[QVX]][A-Z-[IJZ]][0-9][0-9]?)|(([A-Z-[QVX]][0-9][A-HJKPSTUW])|([A-Z-[QVX]][A-Z-[IJZ]][0-9][ABEHMNPRVWXY])))) [0-9][A-Z-[CIKMOV]]{2})"
 
   def geolocate
     # Get the postcode from the location string
     postcode = self.location.match(POSTCODE_REGEX)[0].delete("\s")
+    return false unless postcode
     # Get a lat/lng from postcodes.io
     response = RestClient.get "http://api.postcodes.io/postcodes/#{postcode}"
     result = JSON.parse(response)
-    self.latitude = result['result']['latitude']
-    self.longitude = result['result']['latitude']
-    self.save!
+    puts result
+    return false unless result['result']['latitude']
+    update_column(:latitude, result['result']['latitude'])
+    update_column(:longitude, result['result']['longitude'])
   end
 
   def self.to_csv(options = {})
