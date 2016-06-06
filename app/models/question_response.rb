d440178e60c77e25b8e536d3d702ab067d2ca6b4
Kim Foale <kim@alliscalm.net>
2016-06-06 16:19:25 +0100

    Member pages link properly

--- app/models/question_response.rb
+++ app/models/question_response.rb
@@ -5,4 +5,17 @@ class QuestionResponse < ActiveRecord::Base
 
   validates_presence_of :responder, :question, :subject, :response
 
+
+  def formatted_response
+    response_options = self.question.response
+    if response_options && response_options.length > 0
+      # If there's something here, it's a JSON array of possible responses
+      response_options = JSON.parse(response_options)
+      # So return the correct text string instead of the raw number
+      response_options[self.response.to_i]
+    else
+      # Or, just return if it there's no validation criteria
+      self.response
+    end
+  end
 end
