b1b6eed630b47e128bd62b05cd2a2e74e9ed0437
Kim Foale <kim@alliscalm.net>
2016-06-22 16:25:07 +0100

    Table sorting

--- app/assets/javascripts/partials/_tables.js
+++ app/assets/javascripts/partials/_tables.js
@@ -1,3 +1,23 @@
-// $(function(){
-// 	$('#js-table-sort').dataTable();
-// });
+$(function(){
+  // Setup - add a text input to each footer cell
+  $('.js-table-sort tfoot th.js-table-sort__searchable').each( function () {
+      var title = $(this).text();
+      $(this).html( '<input type="text" size="10" placeholder="Search '+title+'" />' );
+  } );
+
+	var table = $('.js-table-sort').DataTable({
+    pageLength: 25,
+  });
+
+  // Apply the search
+  table.columns().every( function () {
+      var that = this;
+      $( 'input', this.footer() ).on( 'keyup change', function () {
+          if ( that.search() !== this.value ) {
+              that
+                .search( this.value )
+                .draw();
+          }
+      } );
+  } );
+});
