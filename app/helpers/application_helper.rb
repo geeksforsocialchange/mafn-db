6e3eb2d3d8b0d53617132c775f6cba9c77abc035
Kim Foale <kim@alliscalm.net>
2016-06-28 16:55:09 +0100

    Enough for one day

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,5 +11,18 @@ module ApplicationHelper
       "ERROR: Question not found!"
     end
   end
-  
+
+  def last_response_if_in_array(member, question, options)
+    q = Question.where(question: question).pluck(:id)
+    if !q.empty?
+      r = QuestionResponse.where(question: q, responder:member)
+      if !r.empty?
+        response = r.last.formatted_response
+        return response if options.include? response
+      end
+    else
+      "ERROR: Question not found!"
+    end
+  end
+
 end
