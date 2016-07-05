eccd15d0beebf037600c52a57fcabf3bf69f5f3f
Kim Foale <kim@alliscalm.net>
2016-07-05 14:23:47 +0100

    Update tests and outputs

--- test/integration/export/event_test.rb
+++ test/integration/export/event_test.rb
@@ -9,7 +9,7 @@ class ExportEventTest < ActionDispatch::IntegrationTest
   end
 
   test "event spreadsheet export" do
-    
+
     sheet = load_spreadsheet "events"
     head = sheet.row(1)
     content = sheet.row(2)
@@ -21,12 +21,6 @@ class ExportEventTest < ActionDispatch::IntegrationTest
     # TODO: implement project link
     assert_equal head[0], "Project External ID"
     # assert_equal content[0], ""
-
-    # TODO: find out what MAFN want here
-    assert_equal head[4], "Type of Event"
-
-    # TODO: find out what this is for
-    assert_equal head[5], "Other Event"
 
     #======#
     # DONE #
@@ -41,6 +35,12 @@ class ExportEventTest < ActionDispatch::IntegrationTest
 
     assert_equal head[3], "Date Of Event"
     assert_equal content[3], "2016-06-04"
+
+    # TODO: find out what MAFN want here
+    assert_equal head[4], "Type of Event"
+
+    # TODO: find out what this is for
+    assert_equal head[5], "Other Event"
 
     assert_equal head[6], "No of attendees"
     assert_equal content[6], 5
