80a72011f8024c934be865ff46a795fbcc711a31
Kim Foale <kim@alliscalm.net>
2016-07-05 15:08:26 +0100

    Refactor question response method a bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,34 +1,26 @@
 module ApplicationHelper
 
-  def last_response(member, question, entity = false)
+  def get_response(member, question, **opts)
     # Find the question the response relates to
     q = Question.where(question: question).pluck(:id)
     # If it exists...
     if !q.empty?
       # Then check the responses to the question
-      if entity
+      if opts[:entity]
         # If this is questions about a specific thing then check that
-        r = QuestionResponse.where(question: q, responder: member, subject: entity)
+        r = QuestionResponse.where(question: q, responder: member, subject: opts[:entity])
       else
         # If not then just the previous response
         r = QuestionResponse.where(question: q, responder: member)
       end
       # If there are responses, return the last one
       if !r.empty?
-        return r.last.formatted_response
-      end
-    else
-      "ERROR: Question not found!"
-    end
-  end
-
-  def last_response_if_in_array(member, question, options)
-    q = Question.where(question: question).pluck(:id)
-    if !q.empty?
-      r = QuestionResponse.where(question: q, responder:member)
-      if !r.empty?
-        response = r.last.formatted_response
-        return response if options.include? response
+        # If first == true, then get the first answer instead
+        if opts[:first] == true
+          return r.first.formatted_response
+        else
+          return r.last.formatted_response
+        end
       end
     else
       "ERROR: Question not found!"
