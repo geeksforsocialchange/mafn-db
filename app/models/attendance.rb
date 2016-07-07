0e963b9855edaac675121558a2bd276192a40000
Kim Foale <kim@alliscalm.net>
2016-07-07 14:24:17 +0100

    Meeting notes

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,4 +3,5 @@ class Attendance < ActiveRecord::Base
   belongs_to :event
   validates_presence_of :member, :event
   validates_uniqueness_of :member, :scope => :event
+  # TODO: check why this isnt working
 end
