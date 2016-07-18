e0d8c450c54a599fcbd6e9ad9b6dd5566dc59cf8
Kim Foale <kim@alliscalm.net>
2016-07-18 18:28:48 +0100

    Fix authentication test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_user! unless ENV['RAILS_ENV'] = "development"
-  WillPaginate.per_page = 50 
+  before_action :authenticate_user! unless ENV['RAILS_ENV'] == "development" || ENV['RAILS_ENV'] == "test"
+  WillPaginate.per_page = 50
 end
