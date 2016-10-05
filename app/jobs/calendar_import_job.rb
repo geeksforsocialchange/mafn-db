87dbde8e0e4b357599947fd907e9f284139da015
Kim Foale <kim@alliscalm.net>
2016-10-05 16:18:15 +0100

    Add some basic tests

--- app/jobs/calendar_import_job.rb
+++ app/jobs/calendar_import_job.rb
@@ -5,38 +5,43 @@ class CalendarImportJob < ActiveJob::Base
 
   def perform(url)
     uri = URI.parse(url)
-    c = nil
-    uri.open { |f| c = Icalendar.parse(f).first }
-    c.events.each do |e|
-      # Check if it's already in there
-      existing_event = Event.where(google_id: "#{e.uid}").first
-      if existing_event
-        Rails.logger.info "Updating event #{e.summary} (#{e.uid})".colorize(:green)
-        # If it is, just update it
-        existing_event.update(
-                      name: e.summary,
-                      start: e.dtstart,
-                      finish: e.dtend,
-                      description: e.description,
-                      location: e.location.html_safe
-                    )
-      else
-        # Otherwise add it to the DB
-        event = Event.new( name: e.summary,
-                      start: e.dtstart,
-                      finish: e.dtend,
-                      description: e.description,
-                      location: e.location.html_safe,
-                      category: :partnership,
-                      google_id: e.uid
-                    )
-        if event.valid?
-          Rails.logger.info "Adding event #{e.summary} (#{e.uid})".colorize(:green)
-          event.save!
+    return unless uri.scheme == %w(http https)
+    ical = nil
+    uri.open { |f| ical = Icalendar.parse(f).first }
+    if ical
+      ical.events.each do |e|
+        # Check if it's already in there
+        existing_event = Event.where(google_id: "#{e.uid}").first
+        if existing_event
+          Rails.logger.info "Updating event #{e.summary} (#{e.uid})".colorize(:green)
+          # If it is, just update it
+          existing_event.update(
+                        name: e.summary,
+                        start: e.dtstart,
+                        finish: e.dtend,
+                        description: e.description,
+                        location: e.location.html_safe
+                      )
         else
-          Rails.logger.warn "INVALID: Event #{e.summary} (#{e.uid})".colorize(:red)
+          # Otherwise add it to the DB
+          event = Event.new( name: e.summary,
+                        start: e.dtstart,
+                        finish: e.dtend,
+                        description: e.description,
+                        location: e.location.html_safe,
+                        category: :partnership,
+                        google_id: e.uid
+                      )
+          if event.valid?
+            Rails.logger.info "Adding event #{e.summary} (#{e.uid})".colorize(:green)
+            event.save!
+          else
+            Rails.logger.warn "INVALID: Event #{e.summary} (#{e.uid})".colorize(:red)
+          end
         end
       end
+    else
+      Rails.logger.warn "INVALID: Not an ICS feed, or ICS feed contains no events".colorize(:red)
     end
   end
 end
--- test/jobs/calendar_import_job_test.rb
+++ test/jobs/calendar_import_job_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CalendarImportJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @valid_url = "https://calendar.google.com/calendar/ical/d0ok3oc2trd21adadm3b8qaimg%40group.calendar.google.com/public/basic.ics"
+    @invalid_url = "fdsfdsfdsFSDfdsfsd"
+  end
+
+  test "job runs correctly" do
+    CalendarImportJob.perform_now(@valid_url)
+    CalendarImportJob.perform_now(@invalid_url)
+  end
+
 end
