0c2f630f53c757fb3d795b53d023915c6716f0d3
Kim Foale <kim@alliscalm.net>
2016-10-06 17:47:58 +0100

    Calendar logging etc

--- app/jobs/calendar_import_job.rb
+++ app/jobs/calendar_import_job.rb
@@ -13,12 +13,18 @@ class CalendarImportJob < ActiveJob::Base
     ical = uri.open { |f| Icalendar.parse(f).first }
     # If we have something valid
     if ical
+      # VAriables to track what happens
+      created_events = 0
+      updated_events = 0
+      events_with_errors = 0
+      error_log = ""
       # Then run through any event object
       ical.events.each do |e|
         # Check if it's already in there
         existing_event = Event.where(google_id: "#{e.uid}").first
         if existing_event
           Rails.logger.info "Updating event #{e.summary} (#{e.uid})".colorize(:blue)
+          existing_event_copy = existing_event
           # If it is, just update it
           existing_event.update(
                         name: e.summary,
@@ -27,9 +33,11 @@ class CalendarImportJob < ActiveJob::Base
                         description: e.description,
                         location: e.location.html_safe
                       )
+          if existing_event != existing_event_copy
+            updated_events += 1
+          end
         else
           # Otherwise add it to the DB
-
           event = Event.new( name: e.summary,
                         start: e.dtstart,
                         finish: e.dtend,
@@ -42,11 +50,26 @@ class CalendarImportJob < ActiveJob::Base
                       )
           if event.valid?
             Rails.logger.info "Adding event #{e.summary} (#{e.uid})".colorize(:green)
+            created_events += 1
             event.save!
           else
-            Rails.logger.warn "INVALID: Event #{e.summary} (#{e.uid})".colorize(:red)
+            error_message = "INVALID: Event #{e.summary} (#{e.uid})"
+            Rails.logger.warn error_message.colorize(:red)
+            events_with_errors += 1
+            error_log = error_log + "\n" + error_message
           end
         end
+        # Update the time stamp and save
+        log = "#{created_events} added \n#{updated_events} updated \n#{events_with_errors} errors \n"
+        if events_with_errors
+          log += error_log
+          calendar.has_errors = true
+        else
+          calendar.has_errors = false
+        end
+        calendar.log = log
+        calendar.last_imported = Time.now
+        calendar.save!
       end
     else
       Rails.logger.warn "INVALID: Not an ICS feed, or ICS feed contains no events".colorize(:red)
