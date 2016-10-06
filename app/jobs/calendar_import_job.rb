6e88d69100f022a54b77c2fcba42ece4a545339a
Kim Foale <kim@alliscalm.net>
2016-10-06 11:34:08 +0100

    Style changes and message improvements

--- app/jobs/calendar_import_job.rb
+++ app/jobs/calendar_import_job.rb
@@ -4,12 +4,13 @@ class CalendarImportJob < ActiveJob::Base
   queue_as :default
 
   def perform(calendar)
+    Rails.logger.info "Importing #{calendar.project.name}".colorize(:blue)
     # Parse the URL into a Ruby URI object
     uri = URI.parse(calendar.url)
     # Quit unless this starts with http
-    return unless uri.scheme == %w(http https)
-    ical = nil
-    uri.open { |f| ical = Icalendar.parse(f).first }
+    return unless ["http", "https"].include? uri.scheme
+    ical = false
+    ical = uri.open { |f| Icalendar.parse(f).first }
     # If we have something valid
     if ical
       # Then run through any event object
@@ -17,7 +18,7 @@ class CalendarImportJob < ActiveJob::Base
         # Check if it's already in there
         existing_event = Event.where(google_id: "#{e.uid}").first
         if existing_event
-          Rails.logger.info "Updating event #{e.summary} (#{e.uid})".colorize(:green)
+          Rails.logger.info "Updating event #{e.summary} (#{e.uid})".colorize(:blue)
           # If it is, just update it
           existing_event.update(
                         name: e.summary,
@@ -28,13 +29,16 @@ class CalendarImportJob < ActiveJob::Base
                       )
         else
           # Otherwise add it to the DB
+
           event = Event.new( name: e.summary,
                         start: e.dtstart,
                         finish: e.dtend,
                         description: e.description,
                         location: e.location.html_safe,
                         category: :partnership,
-                        google_id: e.uid
+                        google_id: e.uid,
+                        region: calendar.region,
+                        is_funded: calendar.is_funded_default
                       )
           if event.valid?
             Rails.logger.info "Adding event #{e.summary} (#{e.uid})".colorize(:green)
