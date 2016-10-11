8fb683d79e3aa9e56e2ddadab102c0c1492de5dc
Kim Foale <kim@alliscalm.net>
2016-10-11 16:19:42 +0100

    Get basic params working

--- app/api/api.rb
+++ app/api/api.rb
@@ -1,7 +1,12 @@
 class API < Grape::API
+  # Grape initalisation
   version 'v1', using: :header, vendor: 'placecal'
   format :json
   prefix :api
 
+  # Rescue all errors and return as JSON instead
+  rescue_from :all
+
+  # Just returning events for now
   mount Placecal::Events
 end
--- app/api/placecal/events.rb
+++ app/api/placecal/events.rb
@@ -2,17 +2,20 @@ module Placecal
   class Events < Grape::API
     desc 'Gets a list of upcoming events'
     params do
-      optional :id, type: Integer
+      optional :id, type: Integer, default: false
+      optional :start, type: DateTime, default: Time.now
+      optional :region, type: Integer, default: 0
     end
     resource :events do
       get do
         if params[:id]
-          Event.find(params[:id])
+          Event .select(:id, :name, :start, :finish, :description, :created_at, :updated_at, :google_id, :location, :latitude, :longitude, :region)
+                .find(params[:id])
         else
           Event .order(:start)
-                .where("finish > ?", Time.now)
-                .limit(50)
                 .select(:id, :name, :start, :finish, :description, :created_at, :updated_at, :google_id, :location, :latitude, :longitude, :region)
+                .where("finish > ?", params[:start])
+                .limit(50)
         end
       end
     end
