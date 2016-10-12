15ce41096453d26cf20f19b516ed9272873fe0fa
Kim Foale <kim@alliscalm.net>
2016-10-12 15:00:07 +0100

    Finalise basic filter options

--- app/api/api.rb
+++ app/api/api.rb
@@ -2,19 +2,48 @@ require 'grape-swagger'
 
 class API < Grape::API
   # Grape initalisation
-  version 'v1', using: :header, vendor: 'placecal'
+  version 'v1', using: :header, vendor: 'ageapi'
   format :json
   prefix :api
+
+  RESPONSE_LIMIT = 50
 
   # Rescue all errors and return as JSON instead
   rescue_from :all
 
   resource :events do
-    desc "List of upcoming events" do
-      params Entities::Event.documentation
+
+    params do
+      optional :id,         type: Integer,  desc: "Unique key for this API"
+      optional :start,      type: DateTime, desc: "Earliest start time (defaults to now)", default: Time.now
+      optional :finish,     type: DateTime, desc: "Latest finish time"
+      optional :page,       type: Integer,  desc: "Results page offset"
+      optional :region,     type: Integer,  desc: "Region code. 0 - Hulme & Moss Side. 1 - Burnage. 2 - Moston. 3 - Miles Platting."
+      optional :google_id,  type: String,   desc: "Google Calendar ID this is scraped from"
     end
+
+    desc "List of next #{RESPONSE_LIMIT} upcoming events."
     get do
-      @events = Event.limit(20)
+      # Initial query, showing only events starting from now
+      @events = Event.order(:start)
+      # Filter out events before a given start time (default now)
+      if params[:start]
+        start_time = params[:start].to_date rescue false
+      end
+      @events.where!("finish > ?", start_time) if start_time
+      # Filter out events that start after a given finish time
+      if params[:finish]
+        finish_time = params[:finish].to_date rescue false
+        @events.where!("finish < ?", finish_time) if finish_time
+      end
+      # Filter by region code
+      @events.where!(region: params[:region]) if params[:region]
+      # Filter by Google ID
+      @events.where!(google_id: params[:google_id]) if params[:google_id]
+      # Limit to a sensible number and offset, if requested
+      @events.limit!(RESPONSE_LIMIT)
+      @events.offset!(params[:page] * RESPONSE_LIMIT) if params[:page]
+      # Return the fields defined in our entity
       present @events, with: Entities::Event
     end
 
--- app/api/entities.rb
+++ app/api/entities.rb
@@ -2,23 +2,17 @@ require 'grape'
 
 module Entities
   class Event < Grape::Entity
-    # desc 'Gets a list of upcoming events'
-    # params do
-    #   optional :id, type: Integer, default: false
-    #   optional :start, type: DateTime, default: Time.now
-    #   optional :region, type: Integer, default: 0
-    # end
     expose :id, documentation: { type: "Integer", desc: "Unique key for this API" }
     expose :name
     expose :description
-    expose :start, documentation: { type: "DateTime", desc: "Start time" }
-    expose :finish, documentation: { type: "DateTime", desc: "Finish time" }
+    expose :start, documentation: { type: "DateTime", desc: "Earliest start time (defaults to now)", default: Time.now }
+    expose :finish, documentation: { type: "DateTime", desc: "Latest finish time" }
+    expose :region, documentation: { type: "Integer", desc: "Region code. 0 - Hulme & Moss Side. 1 - Burnage. 2 - Moston. 3 - Miles Platting." }
     expose :created_at
     expose :updated_at
     expose :google_id, documentation: { type: "String", desc: "Google Calendar ID this is scraped from" }
     expose :location
     expose :latitude
     expose :longitude
-    expose :region, documentation: { type: "Integer", desc: "Region code" }
   end
 end
